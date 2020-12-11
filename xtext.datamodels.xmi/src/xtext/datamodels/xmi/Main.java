3e526184a9b4fa8a8ef81603d05770c273a64f26
Hoang <ngpbhoang1406@gmail.com>
2020-12-11 19:49:42 +0700

    adding main java project to translate DSL to ecore model

---
+++ xtext.datamodels.xmi/src/xtext/datamodels/xmi/Main.java
@@ -0,0 +1,47 @@
+package xtext.datamodels.xmi;
+
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import com.google.inject.Injector;
+
+import datamodels.DatamodelsPackage;
+import datamodels.impl.DatamodelsPackageImpl;
+import securitymodels.SecuritymodelsPackage;
+import xtext.datamodels.DatamodelsStandaloneSetup;
+import xtext.securitymodels.SecurityModelStandaloneSetup;
+
+public class Main {
+
+	public static final String INPUT_UNIVERSITY_DATAMODEL_PATH = "..\\runtime-EclipseXtext\\uni.dm";
+	public static final String INPUT_UNIVERSITY_SECURITYMODEL_PATH = "..\\runtime-EclipseXtext\\uni.sec";
+	public static final String OUTPUT_UNIVERSITY_DATAMODEL_PATH = "src\\xtext\\datamodels\\xmi\\unidm.xmi";
+	public static final String OUTPUT_UNIVERSITY_SECURITYMODEL_PATH = "src\\xtext\\datamodels\\xmi\\unisec.xmi";
+	
+	public static void main(String[] args) throws IOException {
+//		Injector injector = new DatamodelsStandaloneSetup().createInjectorAndDoEMFRegistration();
+//		ResourceSet resourceSet = injector.getInstance(ResourceSet.class);
+//		resourceSet.getPackageRegistry().put(DatamodelsPackage.eNS_URI, DatamodelsPackage.eINSTANCE);
+//		Resource resource = resourceSet.getResource(URI.createURI(INPUT_UNIVERSITY_DATAMODEL_PATH), true);
+//		resource.load(null);
+//		EcoreUtil.resolveAll(resourceSet);
+//		Resource xmiResource = resourceSet.createResource(URI.createURI(OUTPUT_UNIVERSITY_DATAMODEL_PATH));
+//		xmiResource.getContents().add(resource.getContents().get(0));
+//		xmiResource.save(null);
+		Injector injector = new SecurityModelStandaloneSetup().createInjectorAndDoEMFRegistration();
+		ResourceSet resourceSet = injector.getInstance(ResourceSet.class);
+		resourceSet.getPackageRegistry().put(SecuritymodelsPackage.eNS_URI, SecuritymodelsPackage.eINSTANCE);
+		Resource resource = resourceSet.getResource(URI.createURI(INPUT_UNIVERSITY_SECURITYMODEL_PATH), true);
+		resource.load(null);
+		EcoreUtil.resolveAll(resourceSet);
+		Resource xmiResource = resourceSet.createResource(URI.createURI(OUTPUT_UNIVERSITY_SECURITYMODEL_PATH));
+		xmiResource.getContents().add(resource.getContents().get(0));
+		xmiResource.save(null);
+	}
+
+}
