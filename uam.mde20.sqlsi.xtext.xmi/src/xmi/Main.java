d225868d75a82c43dd0a71ceb265e8d929d86dc3
Hoang <ngpbhoang1406@gmail.com>
2021-01-16 08:49:06 +0700

    Update transformation to XMI

--- uam.mde20.sqlsi.xtext.xmi/src/xmi/Main.java
+++ uam.mde20.sqlsi.xtext.xmi/src/xmi/Main.java
@@ -1,4 +1,5 @@
 package xmi;
+
 import java.io.IOException;
 
 import org.eclipse.emf.common.util.URI;
@@ -15,40 +16,51 @@ import uam.mde20.sqlsi.xtext.DatamodelsStandaloneSetup;
 import uam.mde20.sqlsi.xtext.SecuritymodelsStandaloneSetup;
 
 public class Main {
-	static final String datamodelURI = "";
-	static final String securitymodelURI = "";
+	static final String datamodelURI = "../usecase.university/%s";
+	static final String securitymodelURI = "../usecase.university/%s";
 	static final String XMIFileExtension = "xmi";
-	
-	private static Resource transformDataModelXText2XMI() throws IOException {
-		Injector injector = new DatamodelsStandaloneSetup().createInjectorAndDoEMFRegistration();
-		ResourceSet resourceSet = injector.getInstance(ResourceSet.class);
-		resourceSet.getPackageRegistry().put(DatamodelsPackage.eNS_URI, DatamodelsPackage.eINSTANCE);
-		URI uri = URI.createFileURI(datamodelURI);
-		Resource resource = resourceSet.getResource(uri, true);
-		resource.load(null);
-		EcoreUtil.resolveAll(resourceSet);
-		Resource xmiResource = resourceSet.createResource(uri.trimFileExtension().appendFileExtension(XMIFileExtension));
-		xmiResource.getContents().add(resource.getContents().get(0));
-		xmiResource.save(null);
-		return xmiResource;
+
+	private static void transformDataModelXText2XMI(String args) {
+		try {
+			Injector injector = new DatamodelsStandaloneSetup().createInjectorAndDoEMFRegistration();
+			ResourceSet resourceSet = injector.getInstance(ResourceSet.class);
+			resourceSet.getPackageRegistry().put(DatamodelsPackage.eNS_URI, DatamodelsPackage.eINSTANCE);
+			URI uri = URI.createFileURI(String.format(datamodelURI, args));
+			Resource resource = resourceSet.getResource(uri, true);
+			resource.load(null);
+			EcoreUtil.resolveAll(resourceSet);
+			Resource xmiResource = resourceSet
+					.createResource(uri.trimFileExtension().appendFileExtension(XMIFileExtension));
+			xmiResource.getContents().add(resource.getContents().get(0));
+			xmiResource.save(null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	public static void main(String[] args) throws IOException {
-		transformDataModelXText2XMI();
-		transformSecurityModelXText2XMI();
+	public static void main(String[] args) {
+		transformDataModelXText2XMI(args[0]);
+		transformSecurityModelXText2XMI(args[1]);
 	}
 
-	private static void transformSecurityModelXText2XMI() throws IOException {
-		Injector injector = new SecuritymodelsStandaloneSetup().createInjectorAndDoEMFRegistration();
-		ResourceSet resourceSet = injector.getInstance(ResourceSet.class);
-		resourceSet.getPackageRegistry().put(SecuritymodelsPackage.eNS_URI, SecuritymodelsPackage.eINSTANCE);
-		URI uri = URI.createFileURI(securitymodelURI);
-		Resource resource = resourceSet.getResource(uri, true);
-		resource.load(null);
-		EcoreUtil.resolveAll(resourceSet);
-		SecurityModel secModel = (SecurityModel) resource.getContents().get(0);
-		Resource xmiResource = resourceSet.createResource(uri.trimFileExtension().appendFileExtension(XMIFileExtension));
-		xmiResource.getContents().add(secModel);
-		xmiResource.save(null);
+	private static void transformSecurityModelXText2XMI(String args) {
+		try {
+			Injector injector = new SecuritymodelsStandaloneSetup().createInjectorAndDoEMFRegistration();
+			ResourceSet resourceSet = injector.getInstance(ResourceSet.class);
+			resourceSet.getPackageRegistry().put(SecuritymodelsPackage.eNS_URI, SecuritymodelsPackage.eINSTANCE);
+			URI uri = URI.createFileURI(String.format(securitymodelURI, args));
+			Resource resource = resourceSet.getResource(uri, true);
+			resource.load(null);
+			EcoreUtil.resolveAll(resourceSet);
+			SecurityModel secModel = (SecurityModel) resource.getContents().get(0);
+			Resource xmiResource = resourceSet
+					.createResource(uri.trimFileExtension().appendFileExtension(XMIFileExtension));
+			xmiResource.getContents().add(secModel);
+			xmiResource.save(null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
